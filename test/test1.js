64f781bbf15428c61a6a9c616b052b34a28e39e8
James2166 <jlh3ndr@gmail.com>
2022-02-08 12:55:00 -0700

    before and after commands

--- test/test1.js
+++ test/test1.js
@@ -1,19 +1,36 @@
 var expect = require('chai').expect;
 
+// to see console results click "Debug this test" in testing tab
+
 describe('test_suite1', function(){
+    before('setup code', function(){
+        console.log('Setup Code');
+    });
+
+    after('teardown code', function(){
+        console.log('Teardown code');
+    });
+
+    beforeEach('setup for each test', function(){
+        console.log('Setup code for each test');
+    });
+
+    afterEach('teardown for each test', function(){
+        console.log('Teardown code for each test')
+    });
+
     it('test1', function(){
+        console.log('test1')
         expect(true).to.equal(true);
     });
-});
 
-describe('test_suite2', function(){
-    describe('test_suite3', function(){
-        it('test3', function(){
-            expect(true).to.equal(true);
-        });
-    });
-    
     it('test2', function(){
+        console.log('test2')
+        expect(true).to.equal(true);
+    });
+
+    it('test3', function(){
+        console.log('test3')
         expect(true).to.equal(true);
     });
 });
